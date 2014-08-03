a30e32c157873f0b32c266429b6d7d4dd96d7596
capi_x <capi_x@haibane.org>
2014-08-03 16:23:58 +0200

    GUI fixes

--- qotpnitro/mainwindow.h
+++ qotpnitro/mainwindow.h
@@ -1,3 +1,14 @@
+/**
+ * OTPNitro
+ *
+ *  Copyright 2014 by capi_x <capi_x@haibane.org>
+ *
+ *  Licensed under GNU General Public License 3.0 or later.
+ *  Some rights reserved. See COPYING, AUTHORS.
+ *
+ * @license GPL-3.0 <http://www.gnu.org/licenses/gpl-3.0.txt>
+ */
+
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
